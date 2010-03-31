3144f5dfe6b03158d8c9161910bb4d145e16cf35
Bob Gilligan <gilligan@vyatta.com>
2010-03-30 17:53:48 -0700

    Add a license.

--- gen-templates.pl
+++ gen-templates.pl
@@ -1,5 +1,34 @@
 #!/usr/bin/perl
 
+# Module: gen-templates.pl
+#
+# **** License ****
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# A copy of the GNU General Public License is available as
+# `/usr/share/common-licenses/GPL' in the Debian GNU/Linux distribution
+# or on the World Wide Web at `http://www.gnu.org/copyleft/gpl.html'.
+# You can also obtain it by writing to the Free Software Foundation,
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# This code was originally developed by Vyatta, Inc.
+# Portions created by Vyatta are Copyright (C) 2010 Vyatta, Inc.
+# All Rights Reserved.
+#
+# Author: Bob Gilligan
+# Date: March 2010
+# Description: Script to generate Vyatta templates from an input file.
+#
+# **** End License ****
+
 use strict;
 use warnings;
 
